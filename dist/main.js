9701212ac69c8da904971f408e460829c7476b89
AngelRionCervi <a.rioncervi@gmail.com>
2020-10-06 12:43:45 +0200

    added afk screen

--- dist/main.js
+++ dist/main.js
@@ -1,6 +1,7 @@
 import KioskPlayer from "./Player.js";
 var Main = /** @class */ (function () {
     function Main(medias, options) {
+        var _this = this;
         this.medias = medias;
         this.libOptions = {
             wsAddress: "ws://localhost:8080",
@@ -8,9 +9,39 @@ var Main = /** @class */ (function () {
         Object.assign(this.libOptions, options);
         this.player = new KioskPlayer(this.libOptions);
         this.ws = new WebSocket(this.libOptions.wsAddress);
+        this.afkMsg = this.createAfkTitle();
+        this.isAfk = true;
+        this.afkElapsedTime = 0;
+        this.afkTimer = setInterval(function () {
+            _this.afkElapsedTime++;
+            if (_this.afkElapsedTime >= 20 && !_this.isAfk) {
+                _this.setAfkScreen();
+            }
+        }, 1000);
         this.handleWS();
+        this.setBaseCss();
         this.setBackground();
+        this.setAfkScreen();
     }
+    Main.prototype.createAfkTitle = function () {
+        var el = document.createElement("h1");
+        el.style.textAlign = "center";
+        el.style.paddingTop = "30vh";
+        el.style.color = "white";
+        el.style.margin = "0";
+        el.style.transition = "opacity 0.15s linear";
+        el.style.cursor = "default";
+        el.textContent = "Cliquer pour commencer";
+        return el;
+    };
+    Main.prototype.setBaseCss = function () {
+        document.body.style.margin = "0";
+        document.body.style.padding = "0";
+        document.documentElement.style.width = "100%";
+        document.documentElement.style.height = "100%";
+        document.body.style.width = "100%";
+        document.body.style.height = "100%";
+    };
     Main.prototype.handleWS = function () {
         var _this = this;
         this.ws.onmessage = function (event) {
@@ -39,6 +70,20 @@ var Main = /** @class */ (function () {
         }
         document.body.style.backgroundImage = "";
         document.body.style.backgroundColor = "black";
+        return this;
+    };
+    Main.prototype.setAfkScreen = function () {
+        var _this = this;
+        document.body.appendChild(this.afkMsg);
+        document.body.style.boxShadow = "inset 0px 0px  100px " + window.innerWidth + "px rgba(0,0,0,0.60)";
+        document.body.style.transition = "box-shadow 0.25s linear";
+        this.afkMsg.style.opacity = "1";
+        document.addEventListener("mousedown", function () {
+            _this.afkMsg.style.opacity = "0";
+            document.body.style.boxShadow = "inset 0px 0px  100px " + window.innerWidth + "px rgba(0,0,0,0)";
+            _this.isAfk = false;
+            _this.afkElapsedTime = 0;
+        });
         return this;
     };
     return Main;
--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,10 @@ class Main {
     private player: KioskPlayer;
     private ws: WebSocket;
     private medias: string[];
+    private afkMsg: HTMLHeadingElement;
+    private isAfk: boolean;
+    private afkTimer: any;
+    private afkElapsedTime: number;
     constructor(medias: string[], options: any) {
         this.medias = medias;
         this.libOptions = {
@@ -13,8 +17,40 @@ class Main {
         Object.assign(this.libOptions, options);
         this.player = new KioskPlayer(this.libOptions);
         this.ws = new WebSocket(this.libOptions.wsAddress);
+        this.afkMsg = this.createAfkTitle();
+        this.isAfk = true;
+        this.afkElapsedTime = 0;
+        this.afkTimer = setInterval(() => {
+            this.afkElapsedTime++;
+            if (this.afkElapsedTime >= 20 && !this.isAfk) {
+                this.setAfkScreen();
+            }
+        }, 1000);
         this.handleWS();
+        this.setBaseCss();
         this.setBackground();
+        this.setAfkScreen();
+    }
+
+    private createAfkTitle(): HTMLHeadingElement {
+        const el =  document.createElement("h1");
+        el.style.textAlign = "center";
+        el.style.paddingTop = "30vh";
+        el.style.color = "white";
+        el.style.margin = "0";
+        el.style.transition = "opacity 0.15s linear";
+        el.style.cursor = "default";
+        el.textContent = "Cliquer pour commencer";
+        return el;
+    }
+
+    private setBaseCss() {
+        document.body.style.margin = "0";
+        document.body.style.padding = "0";
+        document.documentElement.style.width = "100%";
+        document.documentElement.style.height = "100%";
+        document.body.style.width = "100%";
+        document.body.style.height = "100%";
     }
 
     private handleWS(): void {
@@ -44,6 +80,20 @@ class Main {
         } 
         document.body.style.backgroundImage = "";
         document.body.style.backgroundColor = "black";
+        return this;
+    }
+
+    public setAfkScreen(): this {
+        document.body.appendChild(this.afkMsg);
+        document.body.style.boxShadow = `inset 0px 0px  100px ${window.innerWidth}px rgba(0,0,0,0.60)`;
+        document.body.style.transition = "box-shadow 0.25s linear";
+        this.afkMsg.style.opacity = "1";
+        document.addEventListener("mousedown", () => {
+            this.afkMsg.style.opacity = "0";
+            document.body.style.boxShadow = `inset 0px 0px  100px ${window.innerWidth}px rgba(0,0,0,0)`;
+            this.isAfk = false;
+            this.afkElapsedTime = 0;
+        })
         return this;
     }
 }
